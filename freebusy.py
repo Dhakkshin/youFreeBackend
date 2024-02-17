3943df494da77c6a2da2cc23af7c952619c4288a
Dhakkshin S R <121537995+Dhakkshin@users.noreply.github.com>
2024-02-17 09:34:40 +0530

    Update freebusy.py

--- freebusy.py
+++ freebusy.py
@@ -4,6 +4,8 @@ import os
 from dotenv import load_dotenv
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
+from google_auth_oauthlib.flow import Flow
+from google.oauth2.credentials import Credentials
 
 def get_access_token():
     load_dotenv() 
@@ -11,29 +13,24 @@ def get_access_token():
     client_secret = os.getenv("CLIENT_SECRET")
     scopes = ["https://www.googleapis.com/auth/calendar.readonly"]
 
-    flow = InstalledAppFlow.from_client_secrets_file(
-        "credentials.json",
-        scopes=scopes
+    flow = Flow.from_client_config(
+        {
+            "web": {
+                "client_id": client_id,
+                "client_secret": client_secret,
+                "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+                "token_uri": "https://oauth2.googleapis.com/token",
+                "redirect_uris": ["urn:ietf:wg:oauth:2.0:oob"],
+                "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+                "client_secret": client_secret,
+            }
+        },
+        scopes=scopes,
     )
-
-    # Print the authorization URL
-    auth_url, _ = flow.authorization_url(prompt='consent')
-    print("Authorization URL:", auth_url)
-
-    # Configure the flow to run in headless mode
     flow.run_local_server()
 
-    # Get the credentials object
-    creds = flow.credentials
-
-    # Print the received authorization response
-    print("Received authorization response:", creds)
-    
-    # Save the credentials to a file
-    with open("token.json", "w") as token_file:
-        token_file.write(creds.to_json())
-
-    return creds.token
+    credentials = flow.credentials
+    return credentials.token
 
 
 def collectBusyTimes(start, end, cal_ids):
